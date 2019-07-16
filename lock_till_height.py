fed6c7276249874e2fe757ee310c636d4e8ea49a
bnagtega <bram.nagtegals@trendminer.com>
2019-07-16 12:03:11 +0200

    add test to verify that account is locked

--- lock_till_height.py
+++ lock_till_height.py
@@ -1,13 +1,17 @@
 import pywaves as py
 
 lock_height = 650000
-script = 'height >= '+str(lock_height)
+script = 'height >= ' + str(lock_height)
 
-py.setNode('http://localhost:6861', 'TN', 'L')
+py.setNode('https://privatenode.blackturtle.eu', 'TN', 'L')
 py.setMatcher('https://privatematcher.blackturtle.eu')
 
+address = py.Address(
+    seed='')
 
-address = py.Address(seed='')
-
-output = address.setScript(script,txFee=100000000)
+output = address.setScript(script, txFee=100000000)
 print(output)
+
+# output from test needs to be : ""u'message': u'Transaction is not allowed by account-script'
+test = address.sendWaves(address, 8000, txFee=2000000)
+print(test)
